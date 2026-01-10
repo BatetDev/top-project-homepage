5c351cf1d73603d843d330ccd6b50354de2967d1
Fran Batet <179341721+BatetDev@users.noreply.github.com>
2026-01-09 21:22:42 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js}', './src/template.html'],
+  theme: {
+    extend: {
+      // Your customizations here
+    },
+  },
+  plugins: [],
+};
